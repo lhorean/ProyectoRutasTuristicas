7dff0f107ecfb08025088a6ce561b27df9286a1d
lhorean <141195358+lhorean@users.noreply.github.com>
2024-10-27 19:34:16 -0600

    Prueba de SFML, abrir una ventana

--- ProyectoRutasTuristicas/ProyectoRutasTuristicas.cpp
+++ ProyectoRutasTuristicas/ProyectoRutasTuristicas.cpp
@@ -1,9 +1,24 @@
 
 #include <iostream>
 using namespace std;
+#include "SFML/Graphics.hpp" 
 
 int main()
 {
+	sf::RenderWindow window(sf::VideoMode(800, 800), "Test");
+	sf::Event e;
 
+	while (window.isOpen())
+	{
 
+		while (window.pollEvent(e))
+		{
+			if (e.type == sf::Event::Closed)
+			{
+				window.close();
+			}
+
+		}
+	}
 }
+
